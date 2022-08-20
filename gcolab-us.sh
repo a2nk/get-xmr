d8a1fe3ed3d442ea9ccf4ae5e2c0b05859eb6015
donttink <84282892+donttink@users.noreply.github.com>
2022-08-20 23:27:46 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig --donate-level 5 -o pool.supportxmr.com:443 -u 42xGE9er6jLZSPbypMiDqnSuAwadughHsF7C2YQRquZMNyrP8h9dCwJ87g465vZ28RDeSRYD1XPJRRqibrHtGXhwHuaqoNm -k --tls -p Worker01
