e07e75e6061375f2ac3917f334950c245aee9ec7
Aank is ME <email@aank.me>
2023-01-21 08:55:21 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar xvzf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-static-x64.tar.gz
+tar xvzf xmrig-6.18.1-linux-static-x64.tar.gz
+cd xmrig-6.18.1
+./xmrig -o xmr.pool.gntl.co.uk:20009 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls -p a2nk
